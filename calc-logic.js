4669301454d77d6fa2a976719de85e41e72ede8b
vipul-khedekar <3kvipul@gmail.com>
2022-03-20 20:29:19 +0530

    queried necessary elements

---
+++ calc-logic.js
@@ -0,0 +1,7 @@
+const operandButtons = document.querySelectorAll(`[data-operand]`);
+const operatorButtons = document.querySelectorAll(`[data-operator]`);
+const allClearButton = document.querySelector(`[data-all-clear]`);
+const deleteButton = document.querySelector(`[data-delete]`);
+const equalsButton = document.querySelector(`[data-equals]`);
+const previousDisplayText = document.querySelector(`[data-display-previous]`);
+const currentDisplayText = document.querySelector(`[data-display-current]`);
